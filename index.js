b98ea601b60ebc4893f9228e59fdb627ef9e9883
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-09 13:53:21 +1300

    pre/post-increment

--- index.js
+++ index.js
@@ -12,10 +12,22 @@ console.log(x**y);
 
 // console.log(x*-y)
 
+//Pre-increment increments by one and then posts x immediately. "(++x)" gives us the new value of x
+
 console.log(x);
 console.log(++x);
 console.log(x);
 console.log(++x);
 console.log(x);
 console.log(++x);
+console.log(x);
+
+//Post increment returns what x was before when we do "(++x)", and then posts the new value of x with "(x)"
+
+console.log(x);
+console.log(x++);
+console.log(x);
+console.log(x++);
+console.log(x);
+console.log(x++);
 console.log(x);
