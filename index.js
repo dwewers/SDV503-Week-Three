2ed23b6154db6ab203f124839cf330381e6e4821
Daniel Ewers <daniel-ewers2@live.nmit.ac.nz>
2020-03-09 14:05:36 +1300

    decrement

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let x = 10;
-let y = 5;
+let y = x;
 
 
 
@@ -22,7 +22,7 @@ console.log(x);
 console.log(++x);
 console.log(x);
 
-//Post increment returns what x was before when we do "(++x)", and then posts the new value of x with "(x)"
+// //Post increment returns what x was before when we do "(++x)", and then posts the new value of x with "(x)"
 
 console.log(x);
 console.log(x++);
@@ -30,4 +30,36 @@ console.log(x);
 console.log(x++);
 console.log(x);
 console.log(x++);
+console.log(x);
+
+
+console.log("Pre - Increment");
+
+y = ++x;
+console.log(x);
+console.log(y);
+
+console.log("Post - Increment");
+
+y = x++;
+console.log(x);
+console.log(y);
+
+//Pre-decrement drcrements value by one, then returns x
+
+console.log(x);
+console.log(--x);
+console.log(x);
+console.log(--x);
+console.log(x);
+console.log(--x);
+console.log(x);
+
+//Post-Decrement returns x, then decrements value by one
+console.log(x);
+console.log(x--);
+console.log(x);
+console.log(x--);
+console.log(x);
+console.log(x--);
 console.log(x);
